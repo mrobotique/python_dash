3c8f90d4a174e1ded12a6d28ab2c668f5d2707a3
mrobotique <32857395+mrobotique@users.noreply.github.com>
2017-10-16 20:56:30 -0500

    Add files via upload

---
+++ webcam_example.py
@@ -0,0 +1,45 @@
+import pygame.camera
+import pygame.image
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+def SendMail(ImgFileName):
+    img_data = open(ImgFileName, 'rb').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Security Camera'
+    msg['From'] = 'miguel.aguas@gmail.com'
+    msg['To'] = 'miguel.aguas@gmail.com'
+
+    text = MIMEText("test")
+    msg.attach(text)
+    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+    msg.attach(image)
+    Server = 'smtp.gmail.com'
+    Port =   587
+    UserName ='miguel.aguas@gmail.com'
+    UserPassword = '@L1730086*'
+    s = smtplib.SMTP(Server, Port)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(UserName, UserPassword)
+    s.sendmail(msg['From'] , msg['To'], msg.as_string())
+    s.quit()
+
+def TakePic():
+    pygame.camera.init()    
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+    cam.start()
+    img = cam.get_image()
+    pygame.image.save(img, "photo.jpg")
+    pygame.camera.quit()
+
+
+TakePic()
+SendMail('/home/pi/dashboard/python/photo.jpg')
+
+
