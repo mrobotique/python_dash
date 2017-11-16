46e3da2a320b74d2e0bb029de4a6905d5a702944
mrobotique <32857395+mrobotique@users.noreply.github.com>
2017-11-16 16:36:47 -0600

    Update TempHumid_ESP8266_Mqtt_WithWebConf.ino

--- Arduino_ESP/TempHumid_ESP8266_Mqtt_WithWebConf/TempHumid_ESP8266_Mqtt_WithWebConf.ino
+++ Arduino_ESP/TempHumid_ESP8266_Mqtt_WithWebConf/TempHumid_ESP8266_Mqtt_WithWebConf.ino
@@ -21,13 +21,12 @@ SimpleDHT11 dht11;
 unsigned long previousMillis = 0;
 const long interval = 1000;           // interval at which to blink (milliseconds)
 
-const char* mqtt_server = "192.168.160.151";
+const char* mqtt_server = "192.168.165.180";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-float temp = 0;
-
+byte temperature = 0;
+byte humidity = 0;
 void tick()
 {
   //toggle state
@@ -44,6 +43,28 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   //entered config mode, make led toggle faster
   ticker.attach(0.2, tick);
 }
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -81,38 +102,74 @@ void setup() {
   digitalWrite(BUILTIN_LED, LOW);
 
   client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
-int ReadDTH() {
+void ReadDTH() {
   // start working...
   Serial.println("=================================");
   Serial.println("Sample DHT11...");
 
   // read without samples.
-  byte temperature = 0;
-  byte humidity = 0;
+
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
-    return -99;
+    temperature = 0;
+    humidity = 0;
   }
 
   Serial.print("Sample OK: ");
   Serial.print((int)temperature); Serial.print(" *C, ");
   Serial.print((int)humidity); Serial.println(" H");
-  return temperature;
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentMillis = millis();
-   client.loop();
+  char msg[50];
+  int value = 0;
+  client.loop();
+  if (!client.connected()) {
+    reconnect();
+  }
   if (currentMillis - previousMillis >= interval) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    int temperature = ReadDTH();
-    Serial.print((int)temperature);
-    client.publish("ha/_temperature1", String(temperature).c_str(),true);
+    ReadDTH();
+
+    value =  (int)temperature;
+    sprintf (msg, "%ld", value);
+    client.publish("/sensor/TR1", msg);
+
+    value = (int)humidity;
+    sprintf (msg, "%ld", value);
+    client.publish("/sensor/HR1", msg);
   }
 
 }
