351f2559d6f5da1f9e0532b62c49e57195f6d0ad
mrobotique <miguel.aguas@gmail.com>
2017-11-16 21:19:15 +0000

    Remote ESP8266 mqtt client Sends temperature and humidity Web portal configurable Will add button to set on request

---
+++ Arduino_ESP/TempHumid_ESP8266_Mqtt_WithWebConf/TempHumid_ESP8266_Mqtt_WithWebConf.ino
@@ -0,0 +1,118 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <PubSubClient.h>
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
+
+//For DTH Sensor
+#include <SimpleDHT.h>
+// for DHT11,
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11;
+
+unsigned long previousMillis = 0;
+const long interval = 1000;           // interval at which to blink (milliseconds)
+
+const char* mqtt_server = "192.168.160.151";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+float temp = 0;
+
+void tick()
+{
+  //toggle state
+  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
+  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //set led pin as output
+  pinMode(BUILTIN_LED, OUTPUT);
+  // start ticker with 0.5 because we start in AP mode and try to connect
+  ticker.attach(0.6, tick);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect()) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+  ticker.detach();
+  //keep LED on
+  digitalWrite(BUILTIN_LED, LOW);
+
+  client.setServer(mqtt_server, 1883);
+}
+
+int ReadDTH() {
+  // start working...
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
+    return -99;
+  }
+
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, ");
+  Serial.print((int)humidity); Serial.println(" H");
+  return temperature;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+   client.loop();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    int temperature = ReadDTH();
+    Serial.print((int)temperature);
+    client.publish("ha/_temperature1", String(temperature).c_str(),true);
+  }
+
+}
